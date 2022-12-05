525527b31efcd146f0f9ff3d90613b65e72236c8
iroid2 <104001652+iroid2@users.noreply.github.com>
2022-12-05 06:58:16 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// alert('working')
+
+
+
+
+// document.addEventListener('keydown',(e)=>{
+//     console.log(e.key)
+//     if(e.key==="ArrowRight"){
+//         console.log('Moving to the right')
+//     }else if(e.key==="ArrowLeft"){
+//         console.log("Moving Left")
+//     }
+// })
