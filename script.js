e996d0137c3c3fea4e92bdf8f32320e22d63015c
iroid2 <104001652+iroid2@users.noreply.github.com>
2022-12-06 16:38:45 +0300

    updated mobo version

--- script.js
+++ script.js
@@ -1,13 +1,33 @@
 // alert('working')
 
-
-
-
-// document.addEventListener('keydown',(e)=>{
-//     console.log(e.key)
-//     if(e.key==="ArrowRight"){
-//         console.log('Moving to the right')
-//     }else if(e.key==="ArrowLeft"){
-//         console.log("Moving Left")
+// $('.owl-carousel').owlCarousel({
+//     loop:true,
+//     margin:5,
+//     responsiveClass:true,
+//     responsive:{
+//         0:{
+//             items:1,
+//             nav:true
+//         },
+//         600:{
+//             items:1,
+//             nav:false
+//         },
+//         1000:{
+//             items:1,
+//             nav:true,
+//             loop:false
+//         }
 //     }
 // })
+
+
+// // document.addEventListener('keydown',(e)=>{
+// //     console.log(e.key)
+// //     if(e.key==="ArrowRight"){
+// //         console.log('Moving to the right')
+// //     }else if(e.key==="ArrowLeft"){
+// //         console.log("Moving Left")
+// //     }
+// // })
+
