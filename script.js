c94f140531318e14a327e5abcf6e9bdafd879d78
iroid2 <104001652+iroid2@users.noreply.github.com>
2022-12-07 16:14:25 +0300

    carousel

--- script.js
+++ script.js
@@ -18,34 +18,34 @@ cancel.addEventListener(("click"),function(e){
 })
 
 
-// $('.owl-carousel').owlCarousel({
-//     loop:true,
-//     margin:5,
-//     responsiveClass:true,
-//     responsive:{
-//         0:{
-//             items:1,
-//             nav:true
-//         },
-//         600:{
-//             items:1,
-//             nav:false
-//         },
-//         1000:{
-//             items:1,
-//             nav:true,
-//             loop:false
-//         }
-//     }
-// })
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:5,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:1,
+            nav:false
+        },
+        1000:{
+            items:1,
+            nav:true,
+            loop:false
+        }
+    }
+})
 
 
-// // document.addEventListener('keydown',(e)=>{
-// //     console.log(e.key)
-// //     if(e.key==="ArrowRight"){
-// //         console.log('Moving to the right')
-// //     }else if(e.key==="ArrowLeft"){
-// //         console.log("Moving Left")
-// //     }
-// // })
+document.addEventListener('keydown',(e)=>{
+    console.log(e.key)
+    if(e.key==="ArrowRight"){
+        console.log('Moving to the right')
+    }else if(e.key==="ArrowLeft"){
+        console.log("Moving Left")
+    }
+})
 
